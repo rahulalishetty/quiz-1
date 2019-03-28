e535b83dee718918c77ecbb59f57a97d90f7385b
rahul alishetty <rahulalishetty1905@gmail.com>
2019-03-28 16:58:32 +0530

    starter

--- pool/src/main/java/com/zemoso/pool/domain/Allocated.java
+++ pool/src/main/java/com/zemoso/pool/domain/Allocated.java
@@ -1,4 +1,16 @@
 package com.zemoso.pool.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Allocated {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer allocationId;
+
+
 }
--- pool/src/main/java/com/zemoso/pool/domain/Request.java
+++ pool/src/main/java/com/zemoso/pool/domain/Request.java
@@ -1,4 +1,27 @@
 package com.zemoso.pool.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name= "request")
 public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer groupId;
+
+    @Column(nullable=false)
+    private Integer groupStrength;
+
+    @Basic(optional = false)
+    @Column(name = "LastTouched", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timeStampRequested;
+
+    @Column(nullable = false)
+    private String statusOfBooking;
+
+
+
 }
