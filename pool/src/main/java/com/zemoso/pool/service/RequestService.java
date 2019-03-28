560a7fa95be10f3191cc681407623a99c5ebcff4
pavanreddy21 <pavanreddy1998@gmail.com>
2019-03-28 18:21:23 +0530

    add service

--- pool/src/main/java/com/zemoso/pool/service/RequestService.java
+++ pool/src/main/java/com/zemoso/pool/service/RequestService.java
@@ -3,7 +3,9 @@ package com.zemoso.pool.service;
 import com.zemoso.pool.Repository.RequestRepository;
 import com.zemoso.pool.domain.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RequestService {
 
     @Autowired
